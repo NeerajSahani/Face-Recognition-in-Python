388ee1105c5e9ce7b281767771fd8aa89be3de19
Neeraj Sahani <39199071+NeerajSahani@users.noreply.github.com>
2018-12-08 12:19:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,12 +11,16 @@ class Face:
         self.faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         self.cam = cv2.VideoCapture(0)
         self.recognizer =  cv2.face.LBPHFaceRecognizer_create()
-        self.recognizer.read('recognizer/trainingdata.yml')
+        try:
+            "if previous data is available then load else pass"
+            self.recognizer.read('recognizer/trainingdata.yml')
+        except:
+            pass
         
         try:
             "if table doesn't exist Create the table"
-            cur.execute("CREATE TABLE FaceRecognition(id INTEGER PRIMARY KEY, name varchar(50))")
-        except Exception as e:
+            self.cursor.execute("CREATE TABLE FaceRecognition(id INTEGER PRIMARY KEY, name varchar(50))")
+        except:
             pass
         
     def register(self):
@@ -26,7 +30,7 @@ class Face:
         sampleNumber = 0
         print('Please smile')
         while(True):
-            img = cam.read()[1]
+            img = self.cam.read()[1]
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             faces = self.faceCascade.detectMultiScale(gray, 1.3, 5)
             for (x, y, w, h) in faces:
@@ -38,6 +42,7 @@ class Face:
             if sampleNumber == 20:
                 print('Done')
                 break
+        cv2.destroyAllWindows()
         self.train()
 
     def train(self):
@@ -54,8 +59,9 @@ class Face:
             cv2.imshow('training', faceNp)
             cv2.waitKey(10)
 
-        self.recognizer.train(faces,ids)
+        self.recognizer.train(faces,np.array(IDs))
         self.recognizer.save('recognizer/trainingdata.yml')
+        cv2.destroyAllWindows()
 
     
 
@@ -73,7 +79,7 @@ class Face:
                     Id = str(z[0])
                 else:
                     Id = "Unknown"
-                cv2.putText(img,str(Id),(x,y+h), cv2.FONT_HERSHEY_COMPLEX, 2, (0, 255, 0), 2)    
+                cv2.putText(img,str(Id),(x,y+h), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)    
 
             cv2.imshow("Face",img)
             if cv2.waitKey(1) == 27:
@@ -89,13 +95,16 @@ if __name__ == '__main__':
     algo = Face()
     choice = int(input("1 -> Register\n2 -> Detection\n3 -> Exit\n\nChoice...."))
     if choice == 1:
-        algo.register()
-        algo.detect()
-        del(algo)
+       algo.register()
+       algo.detect()
+       del(algo)
+            
     elif choice == 2:
-        algo.detect()
-        del(algo)
+       algo.detect()
+       del(algo)
+           
     elif choice == 3:
         del(algo)
+            
     else:
-        print('Wrong Choice')
+       print('Wrong Choice')
